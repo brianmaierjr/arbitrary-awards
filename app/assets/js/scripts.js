94266903e8b7283612499bfc0990272d3e1efef3
Brian Maier Jr <brimaidesigns@gmail.com>
2016-08-04 01:29:50 -0400

    styling sharing

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -89,4 +89,30 @@
     return false;
   });
 
+  // Social Sharing
+  $('.shareTrigger').click(function() {
+    $('#share').toggleClass('active');
+    return false;
+  });
+
+  function windowPopup(url, width, height) {
+    // Calculate the position of the popup so
+    // it’s centered on the screen.
+    var left = (screen.width / 2) - (width / 2),
+        top = (screen.height / 2) - (height / 2);
+
+    window.open(
+      url,
+      "",
+      "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
+    );
+  }
+
+  $(".js-social-share").on("click", function(e) {
+    e.preventDefault();
+
+    windowPopup($(this).attr("href"), 500, 300);
+  });
+
+
 })(jQuery, window, document);
